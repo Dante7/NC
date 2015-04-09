c3b2dcf8da2d894a93513efa5eba99e89e422b40
Dante7 <dante.arkade7@gmail.com>
2015-04-09 02:32:25 -0500

    Zurb incluido

--- static/js/source.js
+++ static/js/source.js
@@ -1,14 +1,14 @@
 var app = angular.module('NC', []);
 
-app.controller("ProdCtrl", function($scope, $http) {
-	$http.get('http://api.ecommerce.next-cloud.mx/v1.0/demo.next-cloud.mx/products').success(function(data) {
+app.controller("ProdCtrl", function ($scope, $http) {
+	$http.get('http://api.ecommerce.next-cloud.mx/v1.0/demo.next-cloud.mx/products').success(function (data) {
 		$scope.prods = data;
 	});
 });
 
-app.controller("CatCtrl", function($scope, $http) {
+app.controller("CatCtrl", function ($scope, $http) {
 	$http.get('http://api.ecommerce.next-cloud.mx/v1.0/demo.next-cloud.mx/menu').success(function (data) {
-		$scope.cat = data;
+		$scope.catalog = data;
 	})
 });
 
