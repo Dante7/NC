0673d676be89988ae8762ddbe4ff813403382067
Dante7 <dante.arkade7@gmail.com>
2015-04-08 20:25:18 -0500

    Cat
    Inicio creacion catalogo

--- static/js/source.js
+++ static/js/source.js
@@ -1,8 +1,15 @@
 var app = angular.module('NC', []);
 
-app.controller("CatCtrl", function($scope, $http) {
+app.controller("ProdCtrl", function($scope, $http) {
 	$http.get('http://api.ecommerce.next-cloud.mx/v1.0/demo.next-cloud.mx/products').success(function(data) {
 		$scope.prods = data;
 	});
 });
+
+app.controller("CatCtrl", function($scope, $http) {
+	$http.get('http://api.ecommerce.next-cloud.mx/v1.0/demo.next-cloud.mx/menu').success(function (data) {
+		$scope.cat = data;
+	})
+});
+
 
