bdf604311d56039e66209fac9b08cc8c38186aa4
Dante7 <dante.arkade7@gmail.com>
2015-04-10 22:49:39 -0500

    Catalogo de productos
    Completado el modulo de catalogo en un 80%
    
    Faltantes
    -Localstorage
    -Ordenamiento

--- static/js/source.js
+++ static/js/source.js
@@ -1,10 +1,24 @@
-var app = angular.module('NC', []);
+"use strict"; 
 
-app.controller("ProdCtrl", function ($scope, $http) {
+var app = angular.module('NC', ['infinite-scroll']);
+
+var ProdCtrl = function($scope, $http){
 	$http.get('http://api.ecommerce.next-cloud.mx/v1.0/demo.next-cloud.mx/products').success(function (data) {
-		$scope.prods = data;
+		$scope.items = data;
+		$scope.prods = [];
+		$scope.counter = 0;
+
+		$scope.loadMore = function () {
+			for (var i = 0; i < 15; i++) {
+				++$scope.counter
+				$scope.prods.push($scope.items[$scope.counter]);
+			};
+		}
+		$scope.loadMore();
 	});
-});
+}
+
+app.controller("ProdCtrl", ProdCtrl);
 
 app.controller("CatCtrl", function ($scope, $http) {
 	$http.get('http://api.ecommerce.next-cloud.mx/v1.0/demo.next-cloud.mx/menu').success(function (data) {
